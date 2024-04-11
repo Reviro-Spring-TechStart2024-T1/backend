ebca5a194808724eac12fa90e0516de2bd6bf806
Talant Kosmosbekov <t1kosmos1@gmail.com>
2024-04-12 00:30:24 +0600

    Custom models for users

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,54 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 
-# Create your models here.
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, name, password=None, role='customer'):
+        if not email:
+            raise ValueError('The email address must be set')
+
+        user = self.model(
+            email=self.normalize_email(email),
+            name=name,
+            role=role,
+        )
+
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, name, password):
+        user = self.create_user(
+            email,
+            password=password,
+            name=name,
+            role='admin',
+        )
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+        return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    USER_ROLES = [
+        ('customer', 'Customer'),
+        ('partner', 'Establishment Partner'),
+        ('admin', 'Administrator'),
+    ]
+
+    email = models.EmailField(unique=True)
+    name = models.CharField(max_length=255)
+    date_of_birth = models.DateField(null=True, blank=True)
+    avatar = models.ImageField(upload_to='accounts/avatars/', null=True, blank=True)
+    role = models.CharField(max_length=10, choices=USER_ROLES, default='customer')
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['name']
+
+    def __str__(self):
+        return self.email
