28b03182d3368a4433f49d1381aa2d296d9cbd48
Talant Kosmosbekov <t1kosmos1@gmail.com>
2024-04-15 00:26:51 +0600

    Create models for orders

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from accounts.models import User
+from establishments.models import Establishment
+from menu.models import MenuItem
+from django.utils import timezone
+# from django.core.exceptions import ValidationError
 
-# Create your models here.
+
+class Order(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="orders")
+    establishment = models.ForeignKey(Establishment, on_delete=models.CASCADE, related_name="orders")
+    menu_item = models.ForeignKey(MenuItem, on_delete=models.CASCADE, related_name="orders")
+    order_date = models.DateTimeField(default=timezone.now)
+
+    # def clean(self):
+    #     if self.user.role != "customer":
+    #         raise ValidationError("Only customers can place orders.")
+    #
+    # def save(self, *args, **kwargs):
+    #     self.clean()
+    #     super().save(*args, **kwargs)
+
+    def __str__(self):
+        return f"Order {self.pk} by {self.user.email} at {self.establishment.name} ordered {self.menu_item.name}"
