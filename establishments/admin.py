1d7585df6610328ff79ddba417891ff4b915cfb8
Talant Kosmosbekov <t1kosmos1@gmail.com>
2024-04-15 17:40:55 +0600

    Add admins to each app

--- establishments/admin.py
+++ establishments/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Establishment
 
-# Register your models here.
+
+class EstablishmentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'phone_number', 'owner')
+    list_filter = ('owner',)
+    search_fields = ('name', 'email', 'phone_number')
+    fieldsets = (
+        (None, {'fields': ('owner', 'name', 'email')}),
+        ('Location', {'fields': ('latitude', 'longitude')}),
+        ('Contact', {'fields': ('phone_number', 'avatar')}),
+        ('Description', {'fields': ('description',)}),
+    )
+
+
+admin.site.register(Establishment, EstablishmentAdmin)
