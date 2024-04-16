2ddc94d9f3b62a1aea2c1ce174f556cc462db6c6
eddy.di <eddy.di.fint@gmail.com>
2024-04-16 14:12:19 +0600

    update user.admin name field to first and second names

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,18 +1,19 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+
 from .models import User
 
 
 class UserAdmin(BaseUserAdmin):
     # Display fields in the list view
-    list_display = ('email', 'name', 'role', 'is_active', 'is_staff')
+    list_display = ('email', 'first_name', 'last_name', 'role', 'is_active', 'is_staff')
     list_filter = ('is_active', 'is_staff', 'role')
     search_fields = ('email', 'name')
     ordering = ('email',)
 
     fieldsets = (
         (None, {'fields': ('email', 'password')}),
-        ('Personal info', {'fields': ('name', 'date_of_birth', 'avatar')}),
+        ('Personal info', {'fields': ('first_name', 'last_name', 'date_of_birth', 'avatar')}),
         ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
         ('Important dates', {'fields': ('last_login',)}),
         ('Role', {'fields': ('role',)}),
@@ -20,7 +21,7 @@ class UserAdmin(BaseUserAdmin):
     add_fieldsets = (
         (None, {
             'classes': ('wide',),
-            'fields': ('email', 'name', 'date_of_birth', 'password1', 'password2', 'role', 'is_active', 'is_staff'),
+            'fields': ('email', 'first_name', 'last_name', 'date_of_birth', 'password1', 'password2', 'role', 'is_active', 'is_staff'),
         }),
     )
     readonly_fields = ('last_login',)
