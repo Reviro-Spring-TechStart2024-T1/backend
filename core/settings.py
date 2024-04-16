3cf68df8d4d59f47b229b2ecded3501f749401a4
eddy.di <eddy.di.fint@gmail.com>
2024-04-16 14:23:53 +0600

    add migrations, update allowed hosts

--- core/settings.py
+++ core/settings.py
@@ -32,7 +32,11 @@ SECRET_KEY = os.environ.get(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', True)  # config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost').split(' ')
+ALLOWED_HOSTS = [
+    'backend-nhs0.onrender.com',
+    '127.0.0.1',
+    'localhost'
+]
 
 
 # Application definition
