0ed6d5b993b5bc2819a1ceb3486193ca3906c218
Talant Kosmosbekov <t1kosmos1@gmail.com>
2024-04-16 13:23:51 +0600

    Add first and last name to the accounts model

--- accounts/models.py
+++ accounts/models.py
@@ -3,13 +3,12 @@ from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, Permis
 
 
 class UserManager(BaseUserManager):
-    def create_user(self, email, name, password=None, role='customer'):
+    def create_user(self, email, password=None, role='customer'):
         if not email:
             raise ValueError('The email address must be set')
 
         user = self.model(
             email=self.normalize_email(email),
-            name=name,
             role=role,
         )
 
@@ -17,11 +16,12 @@ class UserManager(BaseUserManager):
         user.save(using=self._db)
         return user
 
-    def create_superuser(self, email, name, password):
+    def create_superuser(self, email, first_name, last_name, password):
         user = self.create_user(
             email,
+            first_name=first_name,
+            last_name=last_name,
             password=password,
-            name=name,
             role='admin',
         )
         user.is_staff = True
@@ -38,7 +38,8 @@ class User(AbstractBaseUser, PermissionsMixin):
     ]
 
     email = models.EmailField(unique=True)
-    full_name = models.CharField(max_length=255)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
     date_of_birth = models.DateField(null=True, blank=True)
     avatar = models.ImageField(upload_to='accounts/avatars/', null=True, blank=True)
     role = models.CharField(max_length=10, choices=USER_ROLES, default='customer')
@@ -48,7 +49,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     objects = UserManager()
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['full_name']
+    REQUIRED_FIELDS = ['first_name', 'last_name']
 
     def __str__(self):
         return self.email
