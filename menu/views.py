f530574be69d5dde9b33beaea4f8905b3cb0a049
Talant Kosmosbekov <t1kosmos1@gmail.com>
2024-04-17 02:04:02 +0600

    Create generic api views for Menu

--- menu/views.py
+++ menu/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from .models import ItemCategory, Menu, MenuItem
+from .serializers import ItemCategorySerializer, MenuSerializer, MenuItemSerializer
 
-# Create your views here.
+
+class ItemCategoryListCreateView(generics.ListCreateAPIView):
+    queryset = ItemCategory.objects.all()
+    serializer_class = ItemCategorySerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+
+class ItemCategoryDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = ItemCategory.objects.all()
+    serializer_class = ItemCategorySerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+
+class MenuDetailView(generics.RetrieveUpdateAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+
+class MenuItemListCreateView(generics.ListCreateAPIView):
+    queryset = MenuItem.objects.all()
+    serializer_class = MenuItemSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+
+class MenuItemDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = MenuItem.objects.all()
+    serializer_class = MenuItemSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
