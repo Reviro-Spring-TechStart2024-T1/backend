6b61d93710f4ad65d629dfb79fc85915d5bd78dd
Talant Kosmosbekov <t1kosmos1@gmail.com>
2024-04-17 15:56:45 +0600

    Add null, blank-True to first and last name

--- accounts/models.py
+++ accounts/models.py
@@ -62,8 +62,8 @@ class User(AbstractBaseUser, PermissionsMixin):
     ]
 
     email = models.EmailField(unique=True)
-    first_name = models.CharField(max_length=100)
-    last_name = models.CharField(max_length=100)
+    first_name = models.CharField(max_length=100, null=True, blank=True)
+    last_name = models.CharField(max_length=100, null=True, blank=True)
     date_of_birth = models.DateField(null=True, blank=True)
     avatar = models.ImageField(upload_to='accounts/avatars/', null=True, blank=True)
     role = models.CharField(max_length=25, choices=USER_ROLES, default='customer')
@@ -74,7 +74,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     objects = UserManager()
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['first_name', 'last_name']
+    REQUIRED_FIELDS = []
 
     def __str__(self):
         return self.email
