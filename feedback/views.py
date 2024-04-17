ebabed053f463bb30da23240d8e4e3758da01cd4
Talant Kosmosbekov <t1kosmos1@gmail.com>
2024-04-17 19:11:27 +0600

    Create init serializers and views for feedbacks

--- feedback/views.py
+++ feedback/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from .models import Feedback
+from .seializers import FeedbackSerializer
 
-# Create your views here.
+
+class FeedbackListView(generics.ListCreateAPIView):
+    queryset = Feedback.objects.all()
+    serializer_class = FeedbackSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+
+class FeedbackDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Feedback.objects.all()
+    serializer_class = FeedbackSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        """Allow only feedback owner or staff to view and modify feedback"""
+        if self.request.user.is_staff:
+            return Feedback.objects.all()
+        return Feedback.objects.filter(user=self.request.user)
