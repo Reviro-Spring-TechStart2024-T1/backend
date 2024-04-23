a5ef8af2175a95df192ddb5e33d941a8417ac14b
Talant Kosmosbekov <t1kosmos1@gmail.com>
2024-04-23 11:26:26 +0600

    Update menu

--- menu/models.py
+++ menu/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+
 from establishments.models import Establishment
 
 
@@ -6,25 +7,24 @@ class ItemCategory(models.Model):
     name = models.CharField(max_length=255)
 
     class Meta:
-        verbose_name = "Item Category"
-        verbose_name_plural = "Item Categories"
+        verbose_name = 'Item Category'
+        verbose_name_plural = 'Item Categories'
 
     def __str__(self):
         return self.name
 
 
 class Menu(models.Model):
-    establishment = models.OneToOneField(Establishment, on_delete=models.CASCADE, related_name="menu")
+    establishment = models.OneToOneField(Establishment, on_delete=models.CASCADE, related_name='menu')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    qr_code_image = models.ImageField(upload_to='menu/qr_code_images/', blank=True, null=True)
 
     class Meta:
-        verbose_name = "Menu"
-        verbose_name_plural = "Menus"
+        verbose_name = 'Menu'
+        verbose_name_plural = 'Menus'
 
     def __str__(self):
-        return f"Menu of the {self.establishment.name}"
+        return f'Menu of the {self.establishment.name}'
 
 
 class MenuItem(models.Model):
@@ -33,13 +33,25 @@ class MenuItem(models.Model):
     item_category = models.ForeignKey(ItemCategory, on_delete=models.CASCADE, related_name='menu_items')
     price = models.DecimalField(max_digits=8, decimal_places=2)
     description = models.TextField(blank=True)
-    availability_status = models.BooleanField()
-    happy_hour_start = models.TimeField(null=True, blank=True)
-    happy_hour_end = models.TimeField(null=True, blank=True)
+    in_stock = models.PositiveIntegerField(default=0)
 
     class Meta:
-        verbose_name = "Menu Item"
-        verbose_name_plural = "Menu Items"
+        verbose_name = 'Menu Item'
+        verbose_name_plural = 'Menu Items'
 
     def __str__(self):
         return self.name
+
+
+class QrCode(models.Model):
+    menu = models.OneToOneField(Menu, on_delete=models.CASCADE, related_name='qrcode')
+    qr_code_image = models.ImageField(upload_to='menu/qr_code_images/', blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name = 'QR code'
+        verbose_name_plural = 'QR codes'
+
+    def __str__(self):
+        return f'QR code of {self.menu}'
