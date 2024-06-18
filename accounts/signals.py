11690cc064103f6c9488d9eeb215bb0c822e9ff9
eddy.di <eddy.di.fint@gmail.com>
2024-06-18 14:46:52 +0600

    change website title

--- accounts/signals.py
+++ accounts/signals.py
@@ -47,7 +47,7 @@ def password_reset_token_created(sender, instance, reset_password_token, *args, 
 
     msg = EmailMultiAlternatives(
         # title:
-        'Password Reset for {title}'.format(title='Some website title'),
+        'Password Reset for {title}'.format(title='DrinkJoy platform'),
         # message:
         email_plaintext_message,
         # from:
