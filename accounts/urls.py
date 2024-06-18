ab2c6fb13de8478cc0cfb01fea7e5cd1c0d1f1b6
eddy.di <eddy.di.fint@gmail.com>
2024-06-18 18:34:15 +0600

    swagger docs update users tag

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,13 @@
-from django.urls import include, path
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from django.urls import path
 
 from .views import (
     BlockPartnerView,
     ChangePasswordView,
+    CustomResetPasswordConfirmViewSet,
+    CustomResetPasswordRequestTokenViewSet,
+    CustomResetPasswordValidateTokenViewSet,
+    CustomTokenObtainPairView,
+    CustomTokenRefreshView,
     LogoutView,
     PartnerListCreateView,
     UnblockPartnerView,
@@ -17,10 +21,15 @@ urlpatterns = [
     path('register/partner/', PartnerListCreateView.as_view(), name='register_partner'),
     path('profile/', UserProfileView.as_view(), name='profile'),
     path('change-password/', ChangePasswordView.as_view(), name='change_password'),
-    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('token/', CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', CustomTokenRefreshView.as_view(), name='token_refresh'),
     path('logout/', LogoutView.as_view(), name='logout'),
-    path('forgot-password/', include('django_rest_passwordreset.urls', namespace='password_reset')),
+    path('forgot-password/',
+         CustomResetPasswordRequestTokenViewSet.as_view({'post': 'create'}), name='request-token'),
+    path('forgot-password/confirm/',
+         CustomResetPasswordConfirmViewSet.as_view({'post': 'create'}), name='confirm'),
+    path('forgot-password/validate-token/',
+         CustomResetPasswordValidateTokenViewSet.as_view({'post': 'create'}), name='validate-token'),
     path('', UserListView.as_view(), name='users_list'),
     path('partner/block/', BlockPartnerView.as_view(), name='block-partner'),
     path('partner/unblock/', UnblockPartnerView.as_view(), name='unblock-partner'),
--- accounts/views.py
+++ accounts/views.py
@@ -1,12 +1,19 @@
+from django_rest_passwordreset.views import (
+    ResetPasswordConfirmViewSet,
+    ResetPasswordRequestTokenViewSet,
+    ResetPasswordValidateTokenViewSet,
+)
 from drf_spectacular.utils import extend_schema
 from rest_framework import generics, permissions, status, views
 from rest_framework.response import Response
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 from menu.permissions import IsAdminUser
 
 from .models import User
 from .serializers import (
     ChangePasswordSerializer,
+    CustomObtainTokenPairSerializer,
     LogoutSerializer,
     PartnerBlockUnblockSerializer,
     PartnerUserRegisterSerializer,
@@ -21,6 +28,14 @@ class UserRegisterView(generics.CreateAPIView):
     serializer_class = UserRegisterSerializer
     permission_classes = [permissions.AllowAny]
 
+    @extend_schema(
+        summary='Create user',
+        description=(
+            'By default if anyone creating through this endpoint an account for themselves '
+            'will be creating a user instance with `customer` role.\n'
+            '- Permissions: Allowed to anyone.'
+        )
+    )
     def post(self, request):
         serializer = UserRegisterSerializer(data=request.data)
         if serializer.is_valid():
@@ -56,6 +71,36 @@ class UserProfileView(generics.RetrieveUpdateAPIView):
     def get_object(self):
         return self.request.user
 
+    @extend_schema(
+        summary='Get profile',
+        description=(
+            'Show\'s details of a user without passing user id to the endpoint.\n'
+            '- Requires authentication.'
+        )
+    )
+    def get(self, request, *args, **kwargs):
+        return super().get(request, *args, **kwargs)
+
+    @extend_schema(
+        summary='Update profile',
+        description=(
+            'Allows user to update their profile.\n'
+            '- Requires authentication.'
+        )
+    )
+    def put(self, request, *args, **kwargs):
+        return super().put(request, *args, **kwargs)
+
+    @extend_schema(
+        summary='Partially update profile',
+        description=(
+            'Allows user to partially update their profile.\n'
+            '- Requires authentication.'
+        )
+    )
+    def patch(self, request, *args, **kwargs):
+        return super().patch(request, *args, **kwargs)
+
 
 class ChangePasswordView(generics.UpdateAPIView):
     serializer_class = ChangePasswordSerializer
@@ -80,6 +125,28 @@ class ChangePasswordView(generics.UpdateAPIView):
 
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+    @extend_schema(
+        summary='Update password',
+        description=(
+            'Allows authenticated user to update password.\n'
+            '- Requires authentication.\n'
+            '- Permissions: Allowed to anyone.'
+        )
+    )
+    def put(self, request, *args, **kwargs):
+        return super().put(request, *args, **kwargs)
+
+    @extend_schema(
+        summary='Partially update password',
+        description=(
+            'Allows authenticated user to partially update password.\n'
+            '- Requires authentication.\n'
+            '- Permissions: Allowed to anyone.'
+        )
+    )
+    def patch(self, request, *args, **kwargs):
+        return super().patch(request, *args, **kwargs)
+
 
 class LogoutView(generics.GenericAPIView):
     '''
@@ -89,6 +156,14 @@ class LogoutView(generics.GenericAPIView):
     serializer_class = LogoutSerializer
     permission_classes = [permissions.IsAuthenticated]
 
+    @extend_schema(
+        summary='Logout user',
+        description=(
+            'Refresh token has to be passed to log out user and put their refresh token into blacklist.\n'
+            '- Requires authentication.\n'
+            '- Permissions: Allowed to anyone.'
+        )
+    )
     def post(self, request):
         serializer = self.serializer_class(data=request.data)
         serializer.is_valid(raise_exception=True)
@@ -129,6 +204,15 @@ class BlockPartnerView(views.APIView):
     serializer_class = PartnerBlockUnblockSerializer
     permission_classes = [IsAdminUser]
 
+    @extend_schema(
+        summary='Block partner',
+        description=(
+            'Allows admin to block partner, under the hood soft delete\'s manager '
+            'options are utilized to achieve blocking of a partner.\n'
+            '- Requires authentication.\n'
+            '- Permissions: Admin only.'
+        )
+    )
     def patch(self, request, format=None):
         return self.update_block_status(request, True)
 
@@ -154,6 +238,15 @@ class UnblockPartnerView(views.APIView):
     serializer_class = PartnerBlockUnblockSerializer
     permission_classes = [IsAdminUser]
 
+    @extend_schema(
+        summary='Unblock partner',
+        description=(
+            'Allows admin to unblock partner, under the hood soft delete\'s manager '
+            'options are utilized to achieve unblocking of a partner.\n'
+            '- Requires authentication.\n'
+            '- Permissions: Admin only.'
+        )
+    )
     def patch(self, request, format=None):
         return self.update_block_status(request, False)
 
@@ -173,3 +266,57 @@ class UnblockPartnerView(views.APIView):
                 return Response({'error': 'Given user is not of role partner.'})
         except User.DoesNotExist:
             return Response({'error': 'Partner not found'}, status=status.HTTP_404_NOT_FOUND)
+
+
+@extend_schema(
+    summary="Validate token",
+    description=(
+        "This endpoint allows you to validate a reset password token. "
+        "It ensures the token is valid and has not expired.\n"
+        "- Permissions: Allowed to anyone."
+    )
+)
+class CustomResetPasswordValidateTokenViewSet(ResetPasswordValidateTokenViewSet):
+    pass
+
+
+@extend_schema(
+    summary="Confirm reset",
+    description=(
+        "This endpoint allows you to confirm a password reset. "
+        "It takes the token and the new password and updates the user's password.\n"
+        "- Permissions: Allowed to anyone."
+    )
+)
+class CustomResetPasswordConfirmViewSet(ResetPasswordConfirmViewSet):
+    pass
+
+
+@extend_schema(
+    summary="Request token",
+    description=(
+        "This endpoint allows you to request a password reset token. "
+        "You need to provide the email associated with the user account.\n"
+        "- Permissions: Allowed to anyone."
+    )
+)
+class CustomResetPasswordRequestTokenViewSet(ResetPasswordRequestTokenViewSet):
+    pass
+
+
+@extend_schema(
+    summary="Obtain JWT token",
+    description="This endpoint allows you to obtain a new JWT token pair "
+    "(access and refresh token) and users role by providing valid user credentials.\n"
+    "- Permissions: Allowed to anyone."
+)
+class CustomTokenObtainPairView(TokenObtainPairView):
+    serializer_class = CustomObtainTokenPairSerializer
+
+
+@extend_schema(
+    summary="Refresh JWT token",
+    description="This endpoint allows you to refresh your JWT access token using a valid refresh token."
+)
+class CustomTokenRefreshView(TokenRefreshView):
+    pass
