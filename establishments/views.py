8d022bc7bef7057e2dfbcc0c6e833d21e01b6ee3
eddy.di <eddy.di.fint@gmail.com>
2024-06-19 17:47:28 +0600

    add establishments' swagger docs

--- establishments/views.py
+++ establishments/views.py
@@ -1,3 +1,4 @@
+from drf_spectacular.utils import extend_schema
 from rest_framework import filters, generics
 from rest_framework.permissions import IsAuthenticated
 
@@ -8,8 +9,6 @@ from .serializers import (
     EstablishmentMapSerializer,
     EstablishmentSerializer,
 )
-
-# from django_filters.rest_framework import DjangoFilterBackend
 
 
 class EstablishmentListCreateView(generics.ListCreateAPIView):
@@ -22,11 +21,77 @@ class EstablishmentListCreateView(generics.ListCreateAPIView):
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
 
+    @extend_schema(
+        summary='Get establishments',
+        description=(
+            'Allows to authenticated users get list of paginated establishments.\n'
+            '- Requires authentication.\n'
+            '- Permission: Allowed to anyone.'
+        )
+    )
+    def get(self, request, *args, **kwargs):
+        return super().get(request, *args, **kwargs)
+
+    @extend_schema(
+        summary='Create establishment',
+        description=(
+            'Allows partner to create establishment.\n'
+            '- Requires authentication.\n'
+            '- Permission: Partner only.'
+        )
+    )
+    def post(self, request, *args, **kwargs):
+        return super().post(request, *args, **kwargs)
+
 
 class EstablishmentDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Establishment.objects.all()
     serializer_class = EstablishmentSerializer
     permission_classes = [IsPartnerOrReadOnly]
+
+    @extend_schema(
+        summary='Get establishment',
+        description=(
+            'Allows to authenticated users get establishment.\n'
+            '- Requires authentication.\n'
+            '- Permission: Allowed to anyone.'
+        )
+    )
+    def get(self, request, *args, **kwargs):
+        return super().get(request, *args, **kwargs)
+
+    @extend_schema(
+        summary='Update establishment',
+        description=(
+            'Allows partner to update establishment.\n'
+            '- Requires authentication.\n'
+            '- Permission: Partner only.'
+        )
+    )
+    def put(self, request, *args, **kwargs):
+        return super().put(request, *args, **kwargs)
+
+    @extend_schema(
+        summary='Partially update establishment',
+        description=(
+            'Allows partner to partially update establishment.\n'
+            '- Requires authentication.\n'
+            '- Permission: Partner only.'
+        )
+    )
+    def patch(self, request, *args, **kwargs):
+        return super().patch(request, *args, **kwargs)
+
+    @extend_schema(
+        summary='Delete establishment',
+        description=(
+            'Allows partner to delete establishment.\n'
+            '- Requires authentication.\n'
+            '- Permission: Partner only.'
+        )
+    )
+    def delete(self, request, *args, **kwargs):
+        return super().delete(request, *args, **kwargs)
 
 
 class EstablishmentBannerCreateView(generics.CreateAPIView):
@@ -34,11 +99,34 @@ class EstablishmentBannerCreateView(generics.CreateAPIView):
     serializer_class = EstablishmentBannerSerializer
     permission_classes = [IsPartnerOrReadOnly]
 
+    @extend_schema(
+        summary='Create establishments banner',
+        description=(
+            'Allows partner to upload establishments banner. '
+            'Field `url` accepts image, and shows the url to it after processing it on server side.\n'
+            '- Requires authentication.\n'
+            '- Permission: Partner only.'
+        )
+    )
+    def post(self, request, *args, **kwargs):
+        return super().post(request, *args, **kwargs)
+
 
 class EstablishmentBannerDeleteView(generics.DestroyAPIView):
     queryset = EstablishmentBanner.objects.all()
     serializer_class = EstablishmentBannerSerializer
     permission_classes = [IsPartnerOrReadOnly]
+
+    @extend_schema(
+        summary='Delete establishments banner',
+        description=(
+            'Allows partner to delete establishments banner.\n'
+            '- Requires authentication.\n'
+            '- Permission: Partner only.'
+        )
+    )
+    def delete(self, request, *args, **kwargs):
+        return super().delete(request, *args, **kwargs)
 
 
 class EstablishmentMapListView(generics.ListAPIView):
@@ -49,6 +137,16 @@ class EstablishmentMapListView(generics.ListAPIView):
     serializer_class = EstablishmentMapSerializer
     pagination_class = None
     permission_classes = [IsAuthenticated]
+
+    @extend_schema(
+        summary='Get establishments on map',
+        description=(
+            'Allows customers to see all the existing establishments in maps.\n'
+            '- Requires authentication.'
+        )
+    )
+    def get(self, request, *args, **kwargs):
+        return super().get(request, *args, **kwargs)
 
 
 class PartnerEstablishmentListView(generics.ListAPIView):
@@ -61,3 +159,14 @@ class PartnerEstablishmentListView(generics.ListAPIView):
 
     def get_queryset(self):
         return Establishment.objects.filter(owner=self.request.user)
+
+    @extend_schema(
+        summary='Get partners establishments',
+        description=(
+            'Allows partners to see their list of establishments.\n'
+            '- Requires authentication.'
+            '- Permission: Partner only.'
+        )
+    )
+    def get(self, request, *args, **kwargs):
+        return super().get(request, *args, **kwargs)
